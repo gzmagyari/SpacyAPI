648a9d976561778d9529eb3adc8fc3b5fd667262
gaborszenesi <gaborszenesi@gmail.com>
2024-10-03 21:22:27 +0100

    fix

--- main.py
+++ main.py
@@ -79,4 +79,5 @@ async def extract_entities(request: EntityExtractionRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=4747)
+    
